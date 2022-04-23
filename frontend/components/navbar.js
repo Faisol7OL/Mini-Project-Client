5ef5007e72087fab81abfe29856b0fa703834347
Faisol7OL <faisol_henchi_150957@hotmail.com>
2022-04-23 17:41:00 +0700

    Edit NavBar

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -1,16 +1,32 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Navbar = () => (
-    <div className='flex justify-between w-1/5 p-2 mx-2 hover:text-red-600'>
-        <Link href="/"> Home </Link> 
-        <Link href="/profile"> Profile </Link> 
-        <Link href="/register"> Register </Link>  
-        <Link href="/login"> Login </Link> 
-        {/* <Link href="/students">Student</Link>  */}
-        {/* <Link href="/getConfig"> Config </Link> |  */}
-        {/* <Link href="/foo"> Foo </Link> | */}
-        <Link href="/logout"> Logout </Link> 
+  <div className="w-full bg-slate-200 ">
+    <div className="flex justify-between bg-slate-200 py-2  ">
+      <div className="flex mx-2">
+        <div className="hover:text-red-600 mx-1 my-0.5 font-mono text-lg">
+          <Link href="/"> Home </Link> 
+        </div>
+        <a>|</a>       
+        <div className="hover:text-red-600 mx-1 my-0.5 font-mono text-lg">
+          <Link href="/profile"> Profile </Link>{" "}
+        </div>
+      </div>
+      <div className="flex justify-end mr-4">
+        <div className="hover:text-red-600 mx-1 my-0.5 font-mono text-lg">
+          <Link href="/register"> Register </Link>{" "} 
+        </div>
+        <a>|</a>    
+        <div className="hover:text-red-600 mx-1 my-0.5 font-mono text-lg">
+          <Link href="/login"> Login </Link>{" "} 
+        </div>
+        <a>|</a>    
+        <div className="hover:text-red-600 mx-1 my-0.5 font-mono text-lg flex justify-end">
+          <Link href="/logout"> Logout </Link>
+        </div>
+      </div>
     </div>
-)
+  </div>
+);
 
-export default Navbar
+export default Navbar;
